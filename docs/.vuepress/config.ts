f8f621ef1de5807ff32b4f7bd59fe6af32c2cd79
mouli <596280267@qq.com>
2025-02-12 02:13:43 +0800

    配置文件修改

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/MyBlog',
+  base: '/MyBlog/',
   lang: 'zh-CN',
   locales: {
     '/': {
